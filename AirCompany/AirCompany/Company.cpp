ac625c1192887c35fef16c3346f153af4304b723
arseni barsegyan <arseni.barsegyan@gmail.com>
2019-04-11 23:38:33 +0300

    Company class separated to header and source

---
+++ AirCompany/AirCompany/Company.cpp
@@ -0,0 +1,49 @@
+#include "Plane.h"
+#include "Company.h"
+
+void Company::addPlane(Plane* plane) {
+	planes.push_back(plane);
+}
+
+void Company::removePlane(Plane* plane) {
+	for (unsigned i = 0; i < planes.size(); i++) {
+		if (planes.at(i) == plane) {
+			planes.erase(planes.begin() + i);
+		}
+	}
+}
+
+int Company::getTotalCapacity() {
+	return planes.size();
+}
+
+int Company::getTotalCarryingWeight()
+{
+	return 0;
+}
+
+vector<Plane*> Company::sortPlanesByFlyingDistance() {
+
+	std::sort(planes.begin(), planes.end(), comparePlanes);
+}
+
+bool comparePlanes(Plane *a, Plane *b) {
+	return (a->fuelConsumption - b->fuelConsumption);
+}
+
+vector<Plane*> Company::findPlanesByFuelConsumption(int startValue, int endValue) {
+	vector<Plane*> founded;
+	for (int i = 0; i < planes.size(); i++) {
+		if (planes.at(i)->fuelConsumption >= startValue && planes.at(i)->fuelConsumption <= endValue) {
+			founded.push_back(planes.at(i));
+		}
+	}
+	return founded;
+}
+
+int compare(const void * x1, const void * x2)
+{
+	auto p1 = (Plane*)x1;
+	auto p2 = (Plane*)x2;
+	return (int)(p1->getFlyDistance - p2->getFlyDistance());
+}
