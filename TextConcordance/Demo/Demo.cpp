6670593df84a623e878f04992ce8510d8347f34d
abarsegyan <a.barsegyan@godeltech.com>
2019-04-18 17:53:32 +0300

    Demo updated.

--- TextConcordance/Demo/Demo.cpp
+++ TextConcordance/Demo/Demo.cpp
@@ -1,25 +1,21 @@
-// Demo.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include "pch.h"
 #include <iostream>
+#include "Concordance.h"
+#include "DocumentCreator.h"
+#include "TextParser.h"
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	DocumentCreator creator = DocumentCreator("D:\\1.txt", 5);
+	try
+	{
+		Document document = creator.CreateDocumentModel();
+		Concordance concordance = TextParser(document).CreateConcordance();
+		concordance.WriteFile("D:\\2.txt");
+		std::cout << "Concordance have been created successfully";
+	}
+	catch (char *c)
+	{
+		std::cout << "Document doesn't exists. Please check your app settings";
+	}
 }
-
-static void CreateTextConcordance() {
-	//DocumentCreator creator = 
-}
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
