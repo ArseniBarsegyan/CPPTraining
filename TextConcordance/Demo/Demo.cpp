e1192f5d6e42ee0fedcb9af4d92818877517a91c
abarsegyan <a.barsegyan@godeltech.com>
2019-04-19 11:53:05 +0300

    Performance metric

--- TextConcordance/Demo/Demo.cpp
+++ TextConcordance/Demo/Demo.cpp
@@ -3,19 +3,26 @@
 #include "Concordance.h"
 #include "DocumentCreator.h"
 #include "TextParser.h"
+#include <chrono>
 
 int main()
 {
+	auto started = std::chrono::high_resolution_clock::now();
+
 	DocumentCreator creator = DocumentCreator("D:\\1.txt", 5);
 	try
 	{
 		Document document = creator.CreateDocumentModel();
 		Concordance concordance = TextParser(document).CreateConcordance();
 		concordance.WriteFile("D:\\3.txt");
-		std::cout << "Concordance have been created successfully";
+		std::cout << "Concordance have been created successfully" << std::endl;
 	}
 	catch (char *c)
 	{
-		std::cout << "Document doesn't exists. Please check your app settings";
+		std::cout << "Document doesn't exists. Please check your app settings" << std::endl;
 	}
+
+	auto done = std::chrono::high_resolution_clock::now();
+	std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(done - started).count();
+	return 0;
 }
