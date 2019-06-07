653a1ec84bb5808b03459c2dbdc0ee2f176371b3
abarsegyan <a.barsegyan@godeltech.com>
2019-06-07 17:34:33 +0300

    Checking replacement of word

--- TextParser/TextParser/TextParser.cpp
+++ TextParser/TextParser/TextParser.cpp
@@ -11,7 +11,17 @@ int main()
 	auto sentence_items = new std::vector<sentence_item*>();
 	sentence_items->push_back(wrd);
 	sentence_items->push_back(wrd_separator);
-	auto sent = new sentence(sentence_items);
+	sentence* sent = new sentence(sentence_items);
+
+	auto replacement_word = new word(new std::string("replacement"));
+	auto replacement_word2 = new word(new std::string("word"));
+	auto replacement_separator = new word_separator(new std::string(" "));
+	auto replacement_string = new std::vector<sentence_item*>();
+	replacement_string->push_back(replacement_word);
+	replacement_string->push_back(replacement_separator);
+	replacement_string->push_back(replacement_word2);
+
+	sent->replace_all_words_by_length_with_substring(4, replacement_string);
 	std::cout << *sent->get_sentence();
 	delete sent;
 }
