2105f8a7f0d943123717d7d644af9363283a5d6d
abarsegyan <a.barsegyan@godeltech.com>
2019-06-11 13:33:00 +0300

    replacement method fixed. Optimization: managing created objects.

--- TextParser/TextParser/TextParser.cpp
+++ TextParser/TextParser/TextParser.cpp
@@ -6,22 +6,25 @@
 
 int main()
 {
+	// Managing these objects implemented in sentence class
 	sentence_item* wrd_separator = new word_separator(new std::string("@"));
 	sentence_item* wrd = new word(new std::string("test"));
 	auto sentence_items = new std::vector<sentence_item*>();
 	sentence_items->push_back(wrd);
 	sentence_items->push_back(wrd_separator);
-	sentence* sent = new sentence(sentence_items);
+	auto sent = new sentence(sentence_items);
 
-	auto replacement_word = new word(new std::string("replacement"));
-	auto replacement_word2 = new word(new std::string("word"));
-	auto replacement_separator = new word_separator(new std::string(" "));
+	sentence_item* replacement_word = new word(new std::string("replacement"));
+	sentence_item* replacement_word2 = new word(new std::string("word"));
+	sentence_item* replacement_separator = new word_separator(new std::string(" "));
 	auto replacement_string = new std::vector<sentence_item*>();
 	replacement_string->push_back(replacement_word);
 	replacement_string->push_back(replacement_separator);
 	replacement_string->push_back(replacement_word2);
 
 	sent->replace_all_words_by_length_with_substring(4, replacement_string);
+
 	std::cout << *sent->get_sentence();
 	delete sent;
+	sent = nullptr;
 }
--- TextParser/TextParser/sentence.cpp
+++ TextParser/TextParser/sentence.cpp
@@ -60,27 +60,35 @@ std::vector<word*>* sentence::get_all_unique_words(int length)
 // TODO: check this method work
 void sentence::replace_all_words_by_length_with_substring(unsigned long word_length, std::vector<sentence_item*>* items)
 {
-	for (int i = 0; i < sentence_items_->size(); i++)
+	for (auto i = 0; i < sentence_items_->size(); ++i)
 	{
-		auto &item = sentence_items_->at(i);
+		auto& item = *sentence_items_->at(i);
 		const std::type_info& type_info = typeid(item);
 		if (type_info != typeid(word))
 		{
 			continue;
 		}
-		if (item->get_value()->size() != word_length)
+		if (item.get_value()->size() != word_length)
 		{
 			continue;
 		}
-		delete &item;
+
+		// delete object from RAM and set it's pointer to null
+		sentence_items_->at(i) = nullptr;
+		delete& item;
+		
 		auto current_position = i;
 		for (auto &subItem : *items)
 		{
 			auto iter = sentence_items_->begin();
 			sentence_items_->insert(iter + current_position, subItem);
 			current_position++;
+			i++;
 		}
 	}
+	// TODO: is it ok to manage object and it's pointer that came from outside inside this method?	
+	delete items;
+	items = nullptr;
 }
 
 void sentence::replace_all_sentence_words_start_with_consonant(int length)
@@ -92,12 +100,21 @@ std::string* sentence::get_sentence()
 	auto* result = new std::string();
 	for (auto& sentence : *sentence_items_)
 	{
-		result->append(*sentence->get_value());
+		if (sentence != nullptr)
+		{
+			result->append(*sentence->get_value());
+		}		
 	}
 	return result;
 }
 
 sentence::~sentence()
 {
+	for (auto& item : *sentence_items_)
+	{
+		delete item;
+		item = nullptr;
+	}
 	delete this->sentence_items_;
+	sentence_items_ = nullptr;
 }
